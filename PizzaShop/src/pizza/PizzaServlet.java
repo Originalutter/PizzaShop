c577f8188e1d8716c1f5aaa867423f1ea939d5a5
Luis <l.mauricio84@gmail.com>
2014-05-05 17:49:29 +0200

    bla bla

--- PizzaShop/src/pizza/PizzaServlet.java
+++ PizzaShop/src/pizza/PizzaServlet.java
@@ -31,7 +31,7 @@ public class PizzaServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		out.println("Hello woooorld som ar piiip");
+		out.println("Hello woooorld som ar piiip och bla bla ");
 	}
 
 	/**
