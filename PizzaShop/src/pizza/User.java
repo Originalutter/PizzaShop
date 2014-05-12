020d7b176d20e003793112792670bb3120d4aaf5
MathiasMac <Corpra>
2014-05-12 11:41:47 +0200

    Added class User

---
+++ PizzaShop/src/pizza/User.java
@@ -0,0 +1,27 @@
+package pizza;
+
+public class User {
+
+	private String name;
+	private boolean isAdmin;
+	private String email;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
